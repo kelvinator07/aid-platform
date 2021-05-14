3c6d043f825a5622d41af2bad476b4bded47320c
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-05-14 01:31:37 +0100

    Constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const SERVER_API_URL = 'https://aid-platform-rails.herokuapp.com';
-export const ACTION_CABLE_URL = 'ws://aid-platform-rails.herokuapp.com/cable';
+export const ACTION_CABLE_URL = 'wss://aid-platform-rails.herokuapp.com/cable';
 // export const SERVER_API_URL = 'http://localhost:5000';
 // var scheme   = "ws://";
 // var uri      = scheme + window.document.location.host + "/";
